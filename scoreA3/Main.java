706f31080295ee75e4a7e77b16c41b6a8e6c9701
IanFurtado <sapocarnificina@gmail.com>
2025-06-11 00:17:55 -0300

    upload local.date

--- scoreA3/Main.java
+++ scoreA3/Main.java
@@ -3,11 +3,6 @@ package scoreA3;
 public class Main {
     public static void main(String[] args) {
         score2 s = new score2(1000);
-        System.out.println(s.pix());
-
-        score1 s1 = new score1(1000);
-
-        System.out.println(s1.getScore());
 
     }
 }
--- scoreA3/score2.java
+++ scoreA3/score2.java
@@ -1,91 +1,130 @@
 package scoreA3;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
 public class score2 {
     protected int score;
+    private LocalDate dataCriacao;
+    
+    
+    private class RegistroAcao {
+        String tipo;
+        int pontos;
+        LocalDate data;
 
-    public score2(){}
-
-    public score2 (int score) {
-        this.score = score;
-    }
-
-    public String pix () {
-        if (score >= 700) {
-            return "Pix aprovado";
-        } else if (score < 700 && score >= 500){
-            return "Pix  aprovado ALERTA nível de score baixo";
-        } else if (score < 500 && score >= 400) {
-            return "Pix retido para análise manual";
-        } else {
-            return "Pix bloqueado";
+        RegistroAcao(String tipo, int pontos) {
+            this.tipo = tipo;
+            this.pontos = pontos;
+            this.data = LocalDate.now();
         }
     }
 
-    public void pagamentoEmDia () {
+    private List<RegistroAcao> historico = new ArrayList<>();
+
+    public score2() {
+        this.score = 0;
+        this.dataCriacao = LocalDate.now();
+    }
+
+    public score2(int score) {
+        this.score = score;
+        this.dataCriacao = LocalDate.now();
+    }
+
+    
+    public void pagamentoEmDia() {
+        historico.add(new RegistroAcao("pagamentoEmDia", 50));
         score += 50;
     }
 
-    public void debitoAutomatico () {
+    public void debitoAutomatico() {
+        historico.add(new RegistroAcao("debitoAutomatico", 100));
         score += 100;
     }
 
-    public void contaAtivado1Ano () {
+    public void contaAtivado1Ano() {
+        historico.add(new RegistroAcao("contaAtivado1Ano", 200));
         score += 200;
     }
 
-    public void investimentoBanco () {
+    public void investimentoBanco() {
+        historico.add(new RegistroAcao("investimentoBanco", 150));
         score += 150;
     }
 
-    public void receberSalario () {
+    public void receberSalario() {
+        historico.add(new RegistroAcao("receberSalario", 250));
         score += 250;
     }
 
-    public void semEstorno () {
+    public void semEstorno() {
+        historico.add(new RegistroAcao("semEstorno", 200));
         score += 200;
     }
 
-    public void atlCadastro () {
+    public void atlCadastro() {
+        historico.add(new RegistroAcao("atlCadastro", 50));
         score += 50;
     }
 
-    public void fidelidade () {
+    public void fidelidade() {
+        historico.add(new RegistroAcao("fidelidade", 100));
         score += 100;
     }
 
-    public void denuncia () {
-        score -= 500;
+    
+    public void aplicarDecaimento() {
+        score = 0; 
+        LocalDate hoje = LocalDate.now();
+
+        for (RegistroAcao acao : historico) {
+            long anos = ChronoUnit.YEARS.between(acao.data, hoje);
+            double valor = acao.pontos;
+
+            switch (acao.tipo) {
+                case "pagamentoEmDia":
+                case "debitoAutomatico":
+                case "fidelidade":
+                    valor *= Math.pow(0.9, anos); 
+                    break;
+                case "investimentoBanco":
+                    if (anos > 2) {
+                        long anosDecaindo = anos - 2;
+                        valor *= Math.pow(0.9, anosDecaindo);
+                    }
+                    break;
+                case "atlCadastro":
+                    if (anos >= 1) valor = 0; 
+                    break;
+                case "semEstorno":
+                    // 
+                    break;
+                case "receberSalario":
+                    
+                    break;
+                case "contaAtivado1Ano":
+                    
+                    break;
+            }
+
+            score += (int) valor;
+        }
     }
 
-    public void cpfInconsistente () {
-        score -= 300;
+    
+    public void registrarEstorno() {
+        historico.removeIf(acao -> acao.tipo.equals("semEstorno"));
     }
 
-    public void transacaoSuspeita () {
-        score -= 100;
+    
+    public void pararReceberSalario() {
+        historico.removeIf(acao -> acao.tipo.equals("receberSalario"));
     }
 
-    public void comportamentoAtipico () {
-        score -= 150;
-    }
-
-    public void semAutentificacao () {
-        score -= 50;
-    }
-
-    public void atraso() {
-        score -= 150;
-    }
-
-    public void cancelamentoDebitoAutomatico () {
-        score -= 100;
-    }
-
-    public void contaInativa6Meses () {
-        score -= 200;
-    }
-
-    public void historicoEstorno () {
-        score -= 250;
+    public int getScore() {
+        return score;
     }
 }
